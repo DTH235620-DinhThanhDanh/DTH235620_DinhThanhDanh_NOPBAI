5a677c66b68debf9a329392370ea04d17bd0e28b
DTH235620-DinhThanhDanh <danh_dth235620@student.agu.edu.vn>
2025-09-11 21:36:59 +0700

    Kiem Tra

--- BTChuong3/Bai12.py
+++
@@ -1,4 +0,0 @@
-for i in range(1, 10):  # Duyệt từ 1 đến 9
-    print(f"\nBảng cửu chương {i}:")
-    for j in range(1, 11):  # Nhân từ 1 đến 10
-        print(f"{i} x {j} = {i*j}")
--- BTChuong3/Bai16.py
+++
@@ -1,4 +0,0 @@
-for a in range(20, 100, 5):
-    print("*", end='')
-print()
-#16 dau
